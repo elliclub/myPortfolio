eda74f2e06789ed7be8b393e9b78bda144cb2137
Eleonor Dammfors <eleonor.dammfors@gmail.com>
2017-12-12 22:39:11 +0100

    add more content to page and fix layout

--- script.js
+++ script.js
@@ -15,3 +15,15 @@ window.onclick = function(event) {
     }
   }
 }
+
+
+var userFeed = new Instafeed({
+    get: 'user',
+    userId: '33313538',
+    accessToken: '33313538.1677ed0.04ef15157f1c4b36ba88a725e4ee9b25',
+    limit: 1,
+    resolution: 'low_resolution',
+    template: '<div class="instagram-img"><a href="{{link}}"><img src="{{image}}"></a></div><div class="instagram-text"><a href="{{link}}"><p>{{caption}}</p></a></div>'
+});
+
+userFeed.run();
