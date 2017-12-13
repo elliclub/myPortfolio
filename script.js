ee7544de0f7036e16aa6ce730c5594de0dac186c
Eleonor Dammfors <eleonor.dammfors@gmail.com>
2017-12-13 13:58:46 +0100

    fix so that instagram opens in new tab when clicked

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ var userFeed = new Instafeed({
     accessToken: '33313538.1677ed0.04ef15157f1c4b36ba88a725e4ee9b25',
     limit: 1,
     resolution: 'low_resolution',
-    template: '<div class="instagram-img"><a href="{{link}}"><img src="{{image}}"></a></div><div class="instagram-text"><a href="{{link}}"><p>{{caption}}</p></a></div>'
+    template: '<div class="instagram-img"><a href="{{link}}" target="_blank"><img src="{{image}}"></a></div><div class="instagram-text"><a href="{{link}}"><p>{{caption}}</p></a></div>'
 });
 
 userFeed.run();
