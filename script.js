8cc69994734c57d2b810cf9e31ee7e1b1f7a4563
Eleonor Dammfors <eleonor.dammfors@gmail.com>
2017-12-18 21:23:50 +0100

    make the page better in smaller screens

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ var userFeed = new Instafeed({
     accessToken: '33313538.1677ed0.04ef15157f1c4b36ba88a725e4ee9b25',
     limit: 1,
     resolution: 'low_resolution',
-    template: '<div class="instagram-img"><a href="{{link}}" target="_blank"><img src="{{image}}"></a></div><div class="instagram-text"><a href="{{link}}"><p>{{caption}}</p></a></div>'
+    template: '<div><div class="instagram-img"><a href="{{link}}" target="_blank"><img src="{{image}}"></a></div><div class="instagram-text"><a href="{{link}}"><p>{{caption}}</p></a></div></div>'
 });
 
 userFeed.run();
